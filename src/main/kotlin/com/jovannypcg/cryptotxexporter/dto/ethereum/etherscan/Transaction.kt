20fecde4bd194f1e1e4938f1f580d7f461beec2a
Jovanny Cruz <jovannypcg@gmail.com>
2025-07-29 18:00:55 -0600

    Add DTOs to handle Etherescan responses

---
+++ src/main/kotlin/com/jovannypcg/cryptotxexporter/dto/ethereum/etherscan/Transaction.kt
@@ -0,0 +1,68 @@
+package com.jovannypcg.cryptotxexporter.dto.ethereum.etherscan
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.JsonNode
+import com.jovannypcg.cryptotxexporter.model.TransactionType
+import java.math.BigDecimal
+import java.time.Instant
+import com.jovannypcg.cryptotxexporter.model.Transaction as InternalTransaction
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class TransactionsResponse(
+    val status: String,
+    val message: String,
+    val result: JsonNode
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Transaction(
+    val blockNumber: String,
+    val hash: String,
+    val nonce: String?,
+    val blockHash: String?,
+    val transactionIndex: String?,
+    val from: String,
+    val to: String,
+    val value: Double = 0.0,
+    val gas: String,
+    val gasPrice: Double = 0.0,
+    val gasUsed: Double = 0.0,
+    val input: String,
+    val contractAddress: String,
+    val methodId: String?,
+    val tokenName: String?,
+    val tokenSymbol: String?,
+    val tokenId: String?,
+
+    @JsonProperty("timeStamp")
+    val timestamp: Instant,
+
+    @JsonProperty("txreceipt_status")
+    val txReceiptStatus: String?,
+) {
+    fun toInternal(
+        transactionType: TransactionType
+    ): InternalTransaction = InternalTransaction(
+        transactionHash = hash,
+        timestamp = timestamp,
+        from = from,
+        to = to,
+        type = transactionType,
+        assetContractAddress = contractAddress,
+        assetSymbolOrName = tokenSymbol,
+        tokenId = tokenId,
+        amount = BigDecimal.valueOf(value),
+        gasFeeEth = calculateGasFee(gasUsed, gasPrice)
+    )
+
+    fun calculateGasFee(gasUsed: Double, gasPrice: Double): BigDecimal {
+        val weiUsed = BigDecimal(gasUsed)
+        val weiPrice = BigDecimal(gasPrice)
+
+        val totalGasInWei = weiUsed.multiply(weiPrice)
+        val weiToEth = BigDecimal("1000000000000000000")
+
+        return totalGasInWei.divide(weiToEth)
+    }
+}
